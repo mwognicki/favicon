7f645f412ed05fd46a5933cb8af85f7cb502ad44
ArthurHoaro <arthur@hoa.ro>
2023-10-21 20:09:33 -0400

    Use existing baseUrl function instead of parse_url

--- src/Favicon/Favicon.php
+++ src/Favicon/Favicon.php
@@ -209,11 +209,9 @@ class Favicon
 
         // Make sure the favicon is an absolute URL.
         if ($favicon && filter_var($favicon, FILTER_VALIDATE_URL) === false) {
-
             // Make sure that favicons starting with "/" get concatenated with host instead of full URL
             if($favicon[0] === '/') {
-                $parsed = parse_url($url);
-                $favicon = $parsed['scheme'] . '://' . $parsed['host'] . $favicon;
+                $favicon = $this->baseUrl($url) . ltrim($favicon, '/');
             } else {
                 $favicon = rtrim($url, '/') . '/' . ltrim($favicon, '/');
             }
